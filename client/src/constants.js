dfc39137564b786c3174e11a0221c18bf6438af4
danbahrami <dan.bahrami@googlemail.com>
2018-04-17 17:45:06 +0100

    Add team Guacamole

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,5 @@
 export const PROJECTS = {
   TACO: { id: 5, name: 'Taco', icon: '🌮' },
   BURRITO: { id: 36, name: 'Burrito', icon: '🌯' },
+  GUACAMOLE: { id: 3120, name: 'Guacamole', icon: '🥑' },
 };
--- server/server/constants.js
+++ server/server/constants.js
@@ -1,6 +1,7 @@
 const DATASETS = {
   5: 'keeper.goals.taco',
   36: 'keeper.goals.burrito',
+  3120: 'keeper.goals.guacamole',
 };
 
 module.exports = {
