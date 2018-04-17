6ab9567adbd37332a73c7623de0c392959a79025
danbahrami <dan.bahrami@googlemail.com>
2018-04-17 17:44:40 +0100

    Whitelist clubhouse story values

--- server/server/controllers/stories.js
+++ server/server/controllers/stories.js
@@ -17,7 +17,22 @@ const list = (req, res) => {
     json: true
   };
 
-  return request(options).then(stories => res.status(200).send(stories));
+  return request(options).then(stories => {
+    res.status(200).send({
+      ...stories,
+      data: stories.data.map(story => ({
+        id: story.id,
+        app_url: story.app_url,
+        name: story.name,
+        archived: story.archived,
+        blocked: story.blocked,
+        blocker: story.blocker,
+        started: story.started,
+        completed: story.completed,
+        completed_at: story.completed_at,
+      })),
+    });
+  });
 }
 
 module.exports = {
