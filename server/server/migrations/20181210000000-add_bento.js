f92795c7918c27683f7db48e548238cc81799d43
Lisa Hunt <lisa@geckoboard.com>
2018-12-10 11:55:16 +0000

    Add CS Team (bento) to Keeper

---
+++ server/server/migrations/20181210000000-add_bento.js
@@ -0,0 +1,17 @@
+'use strict';
+ module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('teams', [
+      {
+        title: 'Bento',
+        icon: '🍱',
+        dataset: 'keeper.goals.bento',
+        slug: 'bento',
+        projects: [5609],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+   down: (queryInterface, Sequelize) => {}
+};
